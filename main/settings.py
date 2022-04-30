c0f6b7ccca030af3394e2dd061bd33bfa75f4485
whyme-duh <ritikshrestha94@gmail.com>
2022-04-30 21:58:37 +0545

    updated

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get('SECRET_KEY_SETTINGS')
+
+try:
+    SECRET_KEY =os.environ.get('SECRET_KEY_SETTINGS')
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
