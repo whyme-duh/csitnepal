320c19cc1054178dff875123d12d8f1dee6a97d9
whyme-duh <ritikshrestha94@gmail.com>
2022-05-01 13:09:04 +0545

    hehe

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'ote#mer)95(&ui()4othz_@mn#9(0(+p91s4z5h8epe5et+v4^'
+SECRET_KEY = os.environ.get('SECRET_KEY_SETTINGS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
